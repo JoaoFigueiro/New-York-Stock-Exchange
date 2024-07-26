7b6e1062c69c0538d551db5053f2144af3db6459
João Leonardo Comiran Figueiró <93953665+JoaoFigueiro@users.noreply.github.com>
2024-07-26 19:30:16 -0300

    FEAT new_york_stock_exchange

---
+++ new_york_stock_exchange.py
@@ -0,0 +1,26 @@
+import xml.etree.ElementTree
+
+xml_file = 'files/nyse.xml'
+
+try: 
+    nyse = xml.etree.ElementTree.parse(xml_file).getroot()
+except FileNotFoundError: 
+    print('File not Found!')
+
+key_names = ["COMPANY", "LAST", "CHANGE", "MIN", "MAX"]
+key_widths = [50, 10, 10, 10, 10]
+
+for (n, w) in zip(key_names, key_widths):
+    print(n.ljust(w), end=' ')
+
+print()    
+print('-'*95)
+
+for elements in nyse: 
+    print(
+        elements.text.ljust(50),
+        elements.attrib.get("last").ljust(10),
+        elements.attrib.get("change").ljust(10),
+        elements.attrib.get("min").ljust(10),
+        elements.attrib.get("max").ljust(10), sep=' '
+    )
